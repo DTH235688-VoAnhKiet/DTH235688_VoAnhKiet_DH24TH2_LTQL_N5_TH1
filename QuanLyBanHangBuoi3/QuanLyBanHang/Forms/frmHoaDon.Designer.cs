8490437e2688cc7ae70f62593fd6eadde0591da7
DTH235688_VoAnhKiet <kietanhvo312@gmail.com>
2026-02-24 14:48:22 +0700

    Thực Hành b4

---
+++ QuanLyBanHangBuoi3/QuanLyBanHang/Forms/frmHoaDon.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace QuanLyBanHang.Forms
+{
+    partial class frmHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            btnLapHoaDon = new Button();
+            btnInHoaDon = new Button();
+            btnSua = new Button();
+            btnThoat = new Button();
+            btnTimKiem = new Button();
+            btnXuat = new Button();
+            btnXoa = new Button();
+            ID = new DataGridViewTextBoxColumn();
+            HoVaTenNhanVien = new DataGridViewTextBoxColumn();
+            HoVaTenKhachHang = new DataGridViewTextBoxColumn();
+            NgayLap = new DataGridViewTextBoxColumn();
+            TongTienHoaDon = new DataGridViewTextBoxColumn();
+            XemChiTiet = new DataGridViewLinkColumn();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(874, 169);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Danh sách hóa đơn";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { ID, HoVaTenNhanVien, HoVaTenKhachHang, NgayLap, TongTienHoaDon, XemChiTiet });
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 23);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(868, 143);
+            dataGridView1.TabIndex = 0;
+            // 
+            // btnLapHoaDon
+            // 
+            btnLapHoaDon.Location = new Point(18, 220);
+            btnLapHoaDon.Name = "btnLapHoaDon";
+            btnLapHoaDon.Size = new Size(122, 29);
+            btnLapHoaDon.TabIndex = 1;
+            btnLapHoaDon.Text = "Lập hóa đơn...";
+            btnLapHoaDon.UseVisualStyleBackColor = true;
+            // 
+            // btnInHoaDon
+            // 
+            btnInHoaDon.Location = new Point(167, 220);
+            btnInHoaDon.Name = "btnInHoaDon";
+            btnInHoaDon.Size = new Size(104, 29);
+            btnInHoaDon.TabIndex = 2;
+            btnInHoaDon.Text = "In hóa đơn...";
+            btnInHoaDon.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(298, 220);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(94, 29);
+            btnSua.TabIndex = 3;
+            btnSua.Text = "Sửa";
+            btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(540, 220);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(94, 29);
+            btnThoat.TabIndex = 4;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnTimKiem
+            // 
+            btnTimKiem.Location = new Point(661, 220);
+            btnTimKiem.Name = "btnTimKiem";
+            btnTimKiem.Size = new Size(94, 29);
+            btnTimKiem.TabIndex = 5;
+            btnTimKiem.Text = "Tìm kiếm";
+            btnTimKiem.UseVisualStyleBackColor = true;
+            // 
+            // btnXuat
+            // 
+            btnXuat.Location = new Point(782, 220);
+            btnXuat.Name = "btnXuat";
+            btnXuat.Size = new Size(94, 29);
+            btnXuat.TabIndex = 6;
+            btnXuat.Text = "Xuất excel";
+            btnXuat.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(419, 220);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 29);
+            btnXoa.TabIndex = 7;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // ID
+            // 
+            ID.DataPropertyName = "ID";
+            ID.HeaderText = "ID";
+            ID.MinimumWidth = 6;
+            ID.Name = "ID";
+            // 
+            // HoVaTenNhanVien
+            // 
+            HoVaTenNhanVien.DataPropertyName = "HoVaTenNhanVien";
+            HoVaTenNhanVien.HeaderText = "Nhân viên";
+            HoVaTenNhanVien.MinimumWidth = 6;
+            HoVaTenNhanVien.Name = "HoVaTenNhanVien";
+            // 
+            // HoVaTenKhachHang
+            // 
+            HoVaTenKhachHang.DataPropertyName = "HoVaTenKhachHang";
+            HoVaTenKhachHang.HeaderText = "Khách hàng ";
+            HoVaTenKhachHang.MinimumWidth = 6;
+            HoVaTenKhachHang.Name = "HoVaTenKhachHang";
+            // 
+            // NgayLap
+            // 
+            NgayLap.DataPropertyName = "NgayLap";
+            NgayLap.HeaderText = "Ngày lập";
+            NgayLap.MinimumWidth = 6;
+            NgayLap.Name = "NgayLap";
+            // 
+            // TongTienHoaDon
+            // 
+            TongTienHoaDon.DataPropertyName = "TongTienHoaDon";
+            TongTienHoaDon.HeaderText = "Tổng tiền ";
+            TongTienHoaDon.MinimumWidth = 6;
+            TongTienHoaDon.Name = "TongTienHoaDon";
+            // 
+            // XemChiTiet
+            // 
+            XemChiTiet.DataPropertyName = "XemChiTiet";
+            XemChiTiet.HeaderText = "Chi tiết ";
+            XemChiTiet.MinimumWidth = 6;
+            XemChiTiet.Name = "XemChiTiet";
+            XemChiTiet.Resizable = DataGridViewTriState.True;
+            XemChiTiet.SortMode = DataGridViewColumnSortMode.Automatic;
+            // 
+            // frmHoaDon
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(898, 295);
+            Controls.Add(btnXoa);
+            Controls.Add(btnXuat);
+            Controls.Add(btnTimKiem);
+            Controls.Add(btnThoat);
+            Controls.Add(btnSua);
+            Controls.Add(btnInHoaDon);
+            Controls.Add(btnLapHoaDon);
+            Controls.Add(groupBox1);
+            Name = "frmHoaDon";
+            Text = "frmHoaDon";
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private DataGridView dataGridView1;
+        private Button btnLapHoaDon;
+        private Button btnInHoaDon;
+        private Button btnSua;
+        private Button btnThoat;
+        private Button btnTimKiem;
+        private Button btnXuat;
+        private Button btnXoa;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn HoVaTenNhanVien;
+        private DataGridViewTextBoxColumn HoVaTenKhachHang;
+        private DataGridViewTextBoxColumn NgayLap;
+        private DataGridViewTextBoxColumn TongTienHoaDon;
+        private DataGridViewLinkColumn XemChiTiet;
+    }
+}
