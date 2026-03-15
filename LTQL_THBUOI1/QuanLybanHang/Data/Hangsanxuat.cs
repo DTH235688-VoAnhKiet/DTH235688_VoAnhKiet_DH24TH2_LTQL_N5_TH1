ec0195385c8e48d0caca8cee467a089947364b96
DTH235688_VoAnhKiet <kietanhvo312@gmail.com>
2026-03-15 10:00:00 +0700

    Add folder 3 and 4

---
+++ LTQL_THBUOI1/QuanLybanHang/Data/Hangsanxuat.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLybanHang.Data
+{
+    public class Hangsanxuat
+    {
+        public int ID { get; set; }
+
+        public string TenHangSanXuat { get; set; }
+
+        public string TenHangSanXuatKhac { get; set; }
+    }
+
+
+}
